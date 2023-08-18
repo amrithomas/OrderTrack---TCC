f19674189805f347a56bff23b0b9dd556d8e74af
jhordydc <jhordy570@gmail.com>
2023-08-18 16:13:26 -0300

    adição dos arquivos

---
+++ src/pages/tela_principal.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Tela Principal</title>
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <img src="images/logo.png" alt="Logo da Empresa">
+        </div>
+        <nav>
+            <a href="#">Home</a>
+            <a href="#">Funcionário</a>
+            <a href="#">Chamados</a>
+        </nav>
+    </header>
+
+    <main>
+    <div class="main-card">
+            <div class="sub-card">
+                <img src="images/funcionario.png" alt="Funcionário 1">
+                <h3>Nome do Funcionário 1</h3>
+                <p>Cargo 1</p>
+                <a href="#"><img src="images/messagem.png" alt="Ícone de Mensagem"></a>
+            </div>
+            <div class="sub-card">
+                <img src="images/funcionario.png" alt="Funcionário 2">
+                <h3>Nome do Funcionário 2</h3>
+                <p>Cargo 2</p>
+                <a href="#"><img src="images/messagem.png" alt="Ícone de Mensagem"></a>
+            </div>
+            <div class="sub-card">
+                <img src="images/funcionario.png" alt="Funcionário 3">
+                <h3>Nome do Funcionário 3</h3>
+                <p>Cargo 3</p>
+                <a href="#"><img src="images/messagem.png" alt="Ícone de Mensagem"></a>
+            </div>
+            <div class="sub-card">
+                <img src="images/funcionario.png" alt="Funcionário 4">
+                <h3>Nome do Funcionário 4</h3>
+                <p>Cargo 4</p>
+                <a href="#"><img src="images/messagem.png" alt="Ícone de Mensagem"></a>
+            </div>
+            <div class="sub-card">
+                <img src="images/funcionario.png" alt="Funcionário 5">
+                <h3>Nome do Funcionário 5</h3>
+                <p>Cargo 5</p>
+                <a href="#"><img src="images/messagem.png" alt="Ícone de Mensagem"></a>
+            </div>
+            <div class="sub-card">
+                <img src="images/funcionario.png" alt="Funcionário 6">
+                <h3>Nome do Funcionário 6</h3>
+                <p>Cargo 6</p>
+                <a href="#"><img src="images/messagem.png" alt="Ícone de Mensagem"></a>
+            </div>
+            
+            
+        </div>
+    </main>
+
+    <footer>
+        <p>&copy; OrderTech . Todos os direitos reservados.</p>
+    </footer>
+</body>
+</html>
