b9c37d2d650cd8f1a28ddb724f1c0e962bdd54e4
jhordydc <jhordy570@gmail.com>
2023-09-11 14:32:12 -0300

    telaprincipal
    atualização da tela principal 110923

--- src/pages/tela_principal.php
+++ src/pages/tela_principal.php
@@ -1,68 +1,96 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../src/styles/tela_principal/styles.css">
     <title>Tela Principal</title>
-</head>
-<body>
-    <header>
-        <div class="logo">
-            <img src="images/logo.png" alt="Logo da Empresa">
-        </div>
-        <nav>
-            <a href="#">Home</a>
-            <a href="#">Funcionário</a>
-            <a href="#">Chamados</a>
-        </nav>
-    </header>
 
-    <main>
-    <div class="main-card">
-            <div class="sub-card">
-                <img src="images/funcionario.png" alt="Funcionário 1">
-                <h3>Nome do Funcionário 1</h3>
-                <p>Cargo 1</p>
-                <a href="#"><img src="images/messagem.png" alt="Ícone de Mensagem"></a>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="../../assets/images/logo.png" id="logo" alt="Logo" width="30" height="30">
+            </a>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse justify-content-end header" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link links" href="#">Home</a>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle links" href="#" id="chamadosDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Chamados <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+                            </svg>
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="chamadosDropdown">
+                            <a class="dropdown-item" href="#">Lista de Chamados</a>
+                            <a class="dropdown-item" href="#">Abrir Chamado</a>
+                        </div>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle links" href="#" id="funcionariosDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Funcionários <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+                            </svg>
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="funcionariosDropdown">
+                            <a class="dropdown-item" href="#">Lista de Funcionários</a>
+                            <a class="dropdown-item" href="#">Login Funcionário</a>
+                        </div>
+                    </li>
+                </ul>
             </div>
-            <div class="sub-card">
-                <img src="images/funcionario.png" alt="Funcionário 2">
-                <h3>Nome do Funcionário 2</h3>
-                <p>Cargo 2</p>
-                <a href="#"><img src="images/messagem.png" alt="Ícone de Mensagem"></a>
-            </div>
-            <div class="sub-card">
-                <img src="images/funcionario.png" alt="Funcionário 3">
-                <h3>Nome do Funcionário 3</h3>
-                <p>Cargo 3</p>
-                <a href="#"><img src="images/messagem.png" alt="Ícone de Mensagem"></a>
-            </div>
-            <div class="sub-card">
-                <img src="images/funcionario.png" alt="Funcionário 4">
-                <h3>Nome do Funcionário 4</h3>
-                <p>Cargo 4</p>
-                <a href="#"><img src="images/messagem.png" alt="Ícone de Mensagem"></a>
-            </div>
-            <div class="sub-card">
-                <img src="images/funcionario.png" alt="Funcionário 5">
-                <h3>Nome do Funcionário 5</h3>
-                <p>Cargo 5</p>
-                <a href="#"><img src="images/messagem.png" alt="Ícone de Mensagem"></a>
-            </div>
-            <div class="sub-card">
-                <img src="images/funcionario.png" alt="Funcionário 6">
-                <h3>Nome do Funcionário 6</h3>
-                <p>Cargo 6</p>
-                <a href="#"><img src="images/messagem.png" alt="Ícone de Mensagem"></a>
-            </div>
-            
-            
         </div>
+    </nav>
+    <main>
+
+        <div class="main-card">
+            <?php
+            // Loop para gerar os cards dos funcionários
+            for ($i = 1; $i <= 6; $i++) {
+            ?>
+            <div class="sub-card">
+
+                <img src="../../assets/images/telaPrincipal/funcionario.png" alt="Funcionário <?php echo $i; ?>">
+                <h3>Nome do Funcionário <?php echo $i; ?></h3>
+                <p>Cargo <?php echo $i; ?></p>
+                <a href="#"><img src="../../assets/images/telaPrincipal/messagem.png" alt="Ícone de Mensagem"></a>
+
+                <!-- Subcard de sobreposição -->
+
+                <div class="subcard-overlay">
+                    <h3>Detalhes do Funcionário <?php echo $i; ?></h3>
+
+                </div>
+            </div>
+            <?php
+            }
+            ?>
+        </div>
+
     </main>
 
-    <footer>
-        <p>&copy; OrderTech . Todos os direitos reservados.</p>
+     <footer>
+        <p>&copy; OrderTech. Todos os direitos reservados.</p>
     </footer>
+
+    <!-- Incluindo os arquivos JavaScript do Bootstrap (opcional) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
+
 </html>
