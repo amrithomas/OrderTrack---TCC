eb4939cf164e64c85a260a57cd8d160443708af4
jhordydc <jhordy570@gmail.com>
2023-10-16 16:14:13 -0300

    atulização
    proc_chamado

--- src/api/controller/proc_chamado.php
+++ src/api/controller/proc_chamado.php
@@ -1,72 +1,205 @@
+
+
 <?php
+
 include_once('../../../conection.php');
+
 session_start();
 
+ 
+
 $titulo_chamado = filter_input(INPUT_POST, 'titulo_chamado', FILTER_SANITIZE_STRING);
-$urgencia = $_POST['urgencia'];
-$prazo = $_POST['data'];
-$local = $_POST['local'];
+
+$urgencia = filter_input(INPUT_POST, 'urgencia', FILTER_SANITIZE_STRING);
+
+$prazo = filter_input(INPUT_POST, 'data', FILTER_SANITIZE_STRING);
+
+$local = filter_input(INPUT_POST, 'local', FILTER_SANITIZE_STRING);
+
 $funcionario = filter_input(INPUT_POST, 'funcionarios', FILTER_SANITIZE_STRING);
-$descricao_tarefa = filter_input(INPUT_POST, 'item', FILTER_SANITIZE_STRING);
 
-if (isset($_FILES["imagem"]) && $_FILES["imagem"]["error"] === UPLOAD_ERR_OK) {
-    $dir = "img/"; // Certifique-se de que há uma pasta chamada "img" no mesmo diretório deste arquivo PHP.
-    
-    $image = $_FILES["imagem"];
+$descricao_tarefa = filter_input(INPUT_POST, 'descricao_tarefa', FILTER_SANITIZE_STRING);
+
+$data_atual = date("Y-m-d H:i:s");
+
+$status = "PENDENTE";
+
+// Verifique se um arquivo de imagem foi enviado
+
+if (isset($_FILES["img"]) && $_FILES["img"]["error"] === UPLOAD_ERR_OK) {
+
+    // Obtenha os dados da imagem (nome, nome temporário, tipo do arquivo)
+
+    $image = $_FILES["img"];
+
+ 
+
     $tmp_name = $image['tmp_name'];
-    $name = basename($image["name"]);
-    $fileType = strtolower(pathinfo($name, PATHINFO_EXTENSION)); 
-    $name = uniqid();
-    $path = $dir . $name . "." . $fileType;
 
-    $allowTypes = array('jpg', 'png', 'jpeg');
+   
+
+    $name = basename($image["name"]);
+
+ 
+
+    $allowTypes = array('jpg','png','jpeg');
+
+ 
+
+    $fileType = strtolower(pathinfo($name, PATHINFO_EXTENSION));
+
+ 
 
     if (in_array($fileType, $allowTypes) && $image['size'] <= 2097152) {
-        if (!is_dir($dir)) {
-            mkdir($dir, 0755, true);
-        }
-        if (move_uploaded_file($tmp_name, $path)) {
-            echo "Upload de imagem bem-sucedido!";
-        } else {
-            $_SESSION['msg'] = "<center><span style='color:red;'>Erro ao fazer upload da imagem.</span></center>";
-            header('Location: ../../../src/pages/abrir_chamado.php');
-            exit;
-        }
-    } else {
-        if ($image['size'] > 2097152) {   
+
+        // Lê os dados binários da imagem
+
+        $imagem_binaria = file_get_contents($tmp_name);
+
+    }else {
+
+ 
+
+        if ($image['size'] > 2097152) {
+
             $_SESSION['msg'] = "<center><span style='color:red;'>Perfil não cadastrado. Tamanho de imagem não aceita. Máx 2MB.</span></center>";
+
+            header('Location: ../../pages/cadastro_funcionario.php');
+
+            exit; // Encerra o script após redirecionar para evitar processamento adicional
+
         } else {
+
             $_SESSION['msg'] = "<center><span style='color:red;'>Perfil não cadastrado. Apenas arquivos JPG, PNG e JPEG são permitidos.</span></center>";
+
+            header('Location: ../../pages/cadastro_funcionario.php');
+
+            exit; // Encerra o script após redirecionar para evitar processamento adicional
+
         }
-        header('Location: ../../../src/pages/abrir_chamado.php');
-        exit;
+
     }
+
+    // Lê os dados binários da imagem
+
+    $imagem_binaria = file_get_contents($tmp_name);
+
+    // Verifique se a imagem não está vazia
+
+    if (($imagem_binaria)) {
+
+        // Use uma prepared statement para inserir dados no banco de dados
+
+        $sql = "INSERT INTO ordem (SERVICO, PRIORIDADE, ITEM, PRAZO, STATUS, LOCALIZACAO,FOTO,CRIADO) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+        $stmt = mysqli_prepare($conn, $sql);
+
+ 
+
+        // Vincule os parâmetros à declaração SQL
+
+        mysqli_stmt_bind_param($stmt, "ssssssss", $titulo_chamado, $urgencia, $descricao_tarefa, $prazo, $status,$local,$imagem_binaria,$data_atual);
+
+        if (mysqli_stmt_execute($stmt)) {
+
+            $id = mysqli_insert_id($conn);
+
+            $_SESSION['msg'] = "<center><span style='color:blue;'>Perfil Criado com sucesso!</span></center>";
+
+            $_SESSION['id'] = $id;
+
+            header('Location: ../../pages/menu.php');
+
+        } else {
+
+            $_SESSION['msg'] = "<center><span style='color:red;'>Erro ao inserir perfil no banco de dados.</span></center>";
+
+            header('Location: ../../pages/cadastro_funcionario.php');
+
+        }
+
+        // Feche a declaração preparada
+
+        mysqli_stmt_close($stmt);
+
+    } else {
+
+        echo 'Imagem vazia ou inválida';
+
+    }
+
+}else{
+
+   
+
+    $insert_query = "INSERT INTO ordem (SERVICO, PRIORIDADE, ITEM, PRAZO, STATUS, LOCALIZACAO, CRIADO) VALUES (?, ?, ?, ?, ?, ?, NOW())";
+
+    $stmt = $conn->prepare($insert_query);
+
+    $stmt->bind_param('ssssss', $titulo_chamado, $urgencia, $descricao_tarefa, $prazo, $status, $local);
+
+ 
+
+    // $result_usuario = "INSERT INTO ordem (SERVICO, PRIORIDADE,ITEM, PRAZO, STATUS, LOCALIZACAO,FOTO,CRIADO) VALUES ('$titulo_chamado', '$urgencia','$descricao_tarefa','$prazo', 'PENDENTE', '$local', '$path',NOW())";
+
+ 
+
+ 
+
+ 
+
+    if ($stmt->execute()) {
+
+        $ultimoIDInserido = $stmt->insert_id;
+
+   
+
+        echo "Último ID inserido na tabela ordem: " . $ultimoIDInserido . "<br>";
+
+        echo "Valor de \$funcionario: " . $funcionario . "<br>";
+
+ 
+
+ 
+
+   
+
+        $cadastro = mysqli_query($conn, "INSERT INTO rel (FK_ORDEM, FK_FUNCIONARIO) values ('$ultimoIDInserido', '$funcionario')");
+
+   
+
+        if ($cadastro) {
+
+            echo "Registro inserido na tabela rel com sucesso.<br>";
+
+        } else {
+
+            echo "Erro ao inserir registro na tabela rel: " . mysqli_error($conn) . "<br>";
+
+        }
+
+   
+
+        $historico_ordem =  mysqli_query($conn, "INSERT INTO historico_ordem (FK_ORDEM) VALUES ('$ultimoIDInserido')");
+
+   
+
+        if ($historico_ordem) {
+
+            echo "Registro inserido na tabela historico_ordem com sucesso.<br>";
+
+        } else {
+
+            echo "Erro ao inserir registro na tabela historico_ordem: " . mysqli_error($conn) . "<br>";
+
+        }
+
+    } else {
+
+        echo "Erro ao executar a consulta na tabela ordem: " . mysqli_error($conn) . "<br>";
+
+    }
+
 }
 
-
-
-$result_usuario = "INSERT INTO ordem (SERVICO, PRIORIDADE, ITEM, PRAZO, STATUS, LOCALIZACAO, FOTO) VALUES ('$titulo_chamado', '$urgencia','$descricao_tarefa','$prazo', 'PENDENTE', '$local', '$path')";
-
-echo "Query: $result_usuario"; // Depuração
-
-if ($conn->query($result_usuario)) {
-    $ultimoIDInserido = $conn->insert_id;
-
-    $cadastro = mysqli_query($conn, "INSERT INTO rel (FK_ORDEM, FK_FUNCIONARIO) values ('$ultimoIDInserido', '$funcionario')");
-    $historico_ordem =  mysqli_query($conn, "INSERT INTO historico_ordem (FK_ORDEM) VALUES ('$ultimoIDInserido')");
-    
-    
-    $_SESSION['msg'] = "<p style='color:blue;'>Chamado criado com sucesso!</p>";
-    
-    // Redirecionar para a página lista_chamados.php
-    header("Location: ../../../src/pages/abrir_chamado.php");
-    exit();
-} else {
-    echo "Erro ao inserir o registro: " . $conn->error;
-    
-    $_SESSION['msg'] = "<p style='color:red;'>Erro ao criar chamado: " . mysqli_error($conn) . "</p>";
-    // Redirecionar para a página edit_chamado.php
-    header("Location: ../../../src/pages/abrir_chamado.php.php");
-    exit();
-}
 ?>
--- src/pages/relatorio.php
+++ src/pages/relatorio.php
@@ -144,12 +144,12 @@
     </main>
     
     <footer>
-        <p>&copy; ProTask . Todos os direitos reservados.</p>
+        <p>&copy; OrderTech . Todos os direitos reservados.</p>
     </footer>
     <!-- Incluindo os arquivos JavaScript do Bootstrap (opcional) -->
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></scrip>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
 
