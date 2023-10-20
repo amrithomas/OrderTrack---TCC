c1b48f804ecc68593a36d8bcb64201c3d459aec2
Gabrielsalesdealbuquerque <sales.albuque@gmail.com>
2023-10-20 14:57:51 -0300

    ATUALIZANDO MODAL DE LISTAGEM CHAMADOS
    ATUALIZANDO MODAL DE LISTAGEM CHAMADOS

--- src/js/js_modal/aparecemodal.js
+++ src/js/js_modal/aparecemodal.js
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
             // Faz uma solicitação AJAX para buscar os dados do funcionário
             $.ajax({
-                url: '/sistema_os/churras/MeatGolden/sistema_OS/src/api/controller/getFuncionario.php',
+                url: '/sistema_os/sistema_OS/src/api/controller/getFuncionario.php',
                 type: 'GET',
                 data: { funcionarioID: funcionarioId },
                 success: function(data) {
