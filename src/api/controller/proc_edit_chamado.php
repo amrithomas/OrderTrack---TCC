de06e3ea28d8d9adbd5dcf1cc5e682e64aae670d
Gabrielsalesdealbuquerque <sales.albuque@gmail.com>
2023-12-01 17:20:13 -0300

    MANDAR MENSAGEM QUANDO NÃO ALTERAR NADA

--- src/api/controller/proc_edit_chamado.php
+++ src/api/controller/proc_edit_chamado.php
@@ -2,108 +2,54 @@
 session_start();
 include_once("../../../conection.php");
 
-$id = $_SESSION['id'];
-$id_rel = $_SESSION['id_rel'];
+// Verifique se o formulário foi enviado
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $id = $_SESSION['id'];
+    $id_rel = $_SESSION['id_rel'];
 
-$titulo = filter_input(INPUT_POST, 'titulo', FILTER_SANITIZE_STRING);
-$assunto = filter_input(INPUT_POST, 'item', FILTER_SANITIZE_STRING);
-$local = filter_input(INPUT_POST, 'local', FILTER_SANITIZE_STRING);
+    // Obtenha os valores do formulário
+    $tituloNovo = trim(filter_input(INPUT_POST, 'titulo', FILTER_SANITIZE_STRING));
+    $assuntoNovo = trim(filter_input(INPUT_POST, 'item', FILTER_SANITIZE_STRING));
+    $localNovo = trim(filter_input(INPUT_POST, 'local', FILTER_SANITIZE_STRING));
+    $urgenciaNova = filter_input(INPUT_POST, 'urgencia', FILTER_SANITIZE_STRING);
+    $prazoNovo = filter_input(INPUT_POST, 'prazo', FILTER_SANITIZE_STRING);
+    $statusNovo = filter_input(INPUT_POST, 'status', FILTER_SANITIZE_STRING);
 
+    // Consulta para obter os valores atuais do banco de dados
+    $consulta = "SELECT SERVICO, ITEM, LOCALIZACAO, PRIORIDADE, PRAZO, STATUS FROM ordem WHERE ID_ORDEM = '$id'";
+    $resultado = mysqli_query($conn, $consulta);
+    $row = mysqli_fetch_assoc($resultado);
 
-$urgencia = filter_input(INPUT_POST, 'urgencia', FILTER_SANITIZE_STRING);
-$prazo = filter_input(INPUT_POST, 'prazo', FILTER_SANITIZE_STRING);
-$status = filter_input(INPUT_POST, 'status', FILTER_SANITIZE_STRING);
+    // Obtenha os valores atuais do banco de dados
+    $tituloAtual = $row['SERVICO'];
+    $assuntoAtual = $row['ITEM'];
+    $localAtual = $row['LOCALIZACAO'];
+    $urgenciaAtual = $row['PRIORIDADE'];
+    $prazoAtual = $row['PRAZO'];
+    $statusAtual = $row['STATUS'];
 
-$nome_completo = $_POST['funcionario'];
-$nome_sobrenome = explode(" ", $nome_completo);
+    // Verifique se os valores enviados são diferentes dos valores atuais
+    if ($tituloNovo != $tituloAtual || $assuntoNovo != $assuntoAtual || $localNovo != $localAtual || $urgenciaNova != $urgenciaAtual || $prazoNovo != $prazoAtual || $statusNovo != $statusAtual) {
+        // Houve alteração, execute a atualização
+        $update_query = "UPDATE ordem SET SERVICO='$tituloNovo', ITEM='$assuntoNovo', LOCALIZACAO='$localNovo', PRIORIDADE='$urgenciaNova', PRAZO='$prazoNovo', STATUS='$statusNovo' WHERE ID_ORDEM='$id'";
+        
+        $resultado_update = mysqli_query($conn, $update_query);
+        
+        if ($resultado_update) {
+            // Atualização bem-sucedida
+            $alteracao_descricao = "Ordem atualizada: 
+            Título: $tituloNovo,
+            Assunto: $assuntoNovo,
+            Localização: $localNovo,
+            Urgência: $urgenciaNova,
+            Prazo: $prazoNovo,
+            Status: $statusNovo";
 
-$nome = $nome_sobrenome[0];
-$sobrenome = $nome_sobrenome[1];
+            $alteracao_query = "INSERT INTO alteracao_ordem (DATA_ALTERACAO, ALTERACAO, FK_ORDEM) VALUES (NOW(), '$alteracao_descricao', '$id')";
+            $resultado_insercao = mysqli_query($conn, $alteracao_query);
 
-var_dump($titulo);
-var_dump($assunto);
-var_dump($local);
-
-$titulo=trim($titulo);
-$assunto=trim($assunto);
-$local=trim($local);
-
-var_dump($titulo);
-var_dump($assunto);
-var_dump($local);
-
-if(empty($titulo AND $assunto AND $local)){
-    $_SESSION['msg'] = '<div class="notificacao" style="border-left: 6px solid red;">
-    <div class="notificacao-div">
-        <i class="bi bi-x-circle-fill" style="color: red;"></i>
-        <div class="mensagem">
-            <span class="text text-1" style="color: red;">Erro! campos em branco</span>
-        </div>
-    </div>
-    <i class="bi bi-x close" style="color: red;"></i>
-    <div class="tempo tempo_error" style="background-color: #ddd;"></div>
-</div>';
-header('Location: ../../pages/editar_chamado.php?id='.$id);
-exit();
-}
-
-
-
-// Obtendo os valores originais do chamado
-$result_usuario = "SELECT * FROM ordem WHERE ID_ORDEM = '$id'";
-$resultado_usuario = mysqli_query($conn, $result_usuario);
-$row_usuario = mysqli_fetch_assoc($resultado_usuario);
-
-
-$update_query = "UPDATE ordem SET SERVICO='$titulo', ITEM='$assunto', LOCALIZACAO='$local', PRIORIDADE='$urgencia', PRAZO='$prazo', STATUS='$status' WHERE ID_ORDEM='$id'";
-
-$resultado_update = mysqli_query($conn, $update_query);
-
-
-
-if (mysqli_affected_rows($conn)) {
-
-    $alteracao_descricao = "Ordem atualizada: 
-    Título: $titulo,
-    Assunto: $assunto,
-    Localização: $local,
-    Urgência: $urgencia,
-    Prazo: $prazo,
-    Status: $status";
-
-    $alteracao_query = "INSERT INTO alteracao_ordem (DATA_ALTERACAO, ALTERACAO, FK_ORDEM) VALUES (NOW(), '$alteracao_descricao', '$id')";
-    $resultado_insercao = mysqli_query($conn, $alteracao_query);
-
-
-    if ($resultado_insercao) {
-        $_SESSION['msg'] = '<div class="notificacao">
-                            <div class="notificacao-div">
-                                <i class="bi bi-check-lg"></i>
-                                <div class="mensagem">
-                                    <span class="text text-1">Chamado Editado com Sucesso</span>
-                                </div>
-                            </div>
-                            <i class="bi bi-x close"></i>
-                            <div class="tempo"></div>
-                        </div>';
-        header("Location: ../../pages/lista_chamados.php");
-        exit;
-    }else{
-         $_SESSION['msg'] = '<div class="notificacao" style="border-left: 6px solid red;">
-        <div class="notificacao-div">
-            <i class="bi bi-x-circle-fill" style="color: red;"></i>
-            <div class="mensagem">
-                <span class="text text-1" style="color: red;">Chamado não Editado!</span>
-            </div>
-        </div>
-        <i class="bi bi-x close" style="color: red;"></i>
-        <div class="tempo tempo_error" style="background-color: #ddd;"></div>
-    </div>';
-    header("Location: ../../pages/editar_chamado.php?id=".$id);
-    }
-
-    
-    $_SESSION['msg'] = '<div class="notificacao">
+            if ($resultado_insercao) {
+                $_SESSION['msg'] = '<div class="notificacao">
                                     <div class="notificacao-div">
                                         <i class="bi bi-check-lg"></i>
                                         <div class="mensagem">
@@ -113,52 +59,49 @@ if (mysqli_affected_rows($conn)) {
                                     <i class="bi bi-x close"></i>
                                     <div class="tempo"></div>
                                 </div>';
-            
-    header("Location: ../../pages/lista_chamados.php");
-    exit;
-} else {
-    $_SESSION['msg'] = '<div class="notificacao" style="border-left: 6px solid red;">
-            <div class="notificacao-div">
-                <i class="bi bi-x-circle-fill" style="color: red;"></i>
-                <div class="mensagem">
-                    <span class="text text-1" style="color: red;">Chamado não Editado!</span>
+                header("Location: ../../pages/lista_chamados.php");
+                exit;
+            } else {
+                $_SESSION['msg'] = '<div class="notificacao" style="border-left: 6px solid red;">
+                <div class="notificacao-div">
+                    <i class="bi bi-x-circle-fill" style="color: red;"></i>
+                    <div class="mensagem">
+                        <span class="text text-1" style="color: red;">Chamado não Editado!</span>
+                    </div>
                 </div>
-            </div>
-            <i class="bi bi-x close" style="color: red;"></i>
-            <div class="tempo tempo_error" style="background-color: #ddd;"></div>
-        </div>';
-    header("Location: ../../pages/editar_chamado.php?id=".$id);
+                <i class="bi bi-x close" style="color: red;"></i>
+                <div class="tempo tempo_error" style="background-color: #ddd;"></div>
+            </div>';
+                header("Location: ../../pages/editar_chamado.php?id=".$id);
+                exit;
+            }
+        } else {
+            $_SESSION['msg'] = '<div class="notificacao" style="border-left: 6px solid red;">
+                <div class="notificacao-div">
+                    <i class="bi bi-x-circle-fill" style="color: red;"></i>
+                    <div class="mensagem">
+                        <span class="text text-1" style="color: red;">Chamado não Editado!</span>
+                    </div>
+                </div>
+                <i class="bi bi-x close" style="color: red;"></i>
+                <div class="tempo tempo_error" style="background-color: #ddd;"></div>
+            </div>';
+            header("Location: ../../pages/editar_chamado.php?id=".$id);
+            exit;
+        }
+    }
+    $_SESSION['msg'] = '<div class="notificacao" style="border-left: 6px solid red;">
+                <div class="notificacao-div">
+                    <i class="bi bi-x-circle-fill" style="color: red;"></i>
+                    <div class="mensagem">
+                        <span class="text text-1" style="color: red;">Nenhum campo alterado</span>
+                    </div>
+                </div>
+                <i class="bi bi-x close" style="color: red;"></i>
+                <div class="tempo tempo_error" style="background-color: #ddd;"></div>
+            </div>';
+    // Se não houver alterações, redirecione de volta para a página de edição sem executar a atualização
+    header('Location: ../../pages/lista_chamados.php');
+    exit();
 }
-
-if(isset($nome_completo) && !empty($nome_completo) && $nome_completo != " "){
-    $select_funcionario = "SELECT * from funcionarios WHERE NOME_FUNCIONARIO = '$nome'";
-    $query_funcionario = mysqli_query($conn, $select_funcionario);
-    $resultado_funcionario = mysqli_fetch_assoc($query_funcionario);
-    
-    $id_funcionario = $resultado_funcionario['ID_FUNCIONARIO'];
-    
-    $update_query_funcionario = "UPDATE rel SET FK_FUNCIONARIO='$id_funcionario' WHERE ID_REL = '$id_rel'";
-    $query_update_funcionario = mysqli_query($conn, $update_query_funcionario);
-    
-    if (mysqli_affected_rows($conn)) {
-        $_SESSION['msg'] = '<div class="notificacao">
-                            <div class="notificacao-div">
-                                <i class="bi bi-check-lg"></i>
-                                <div class="mensagem">
-                                    <span class="text text-1">Chamado Editado com Sucesso</span>
-                                </div>
-                            </div>
-                            <i class="bi bi-x close"></i>
-                            <div class="tempo"></div>
-                        </div>';
-
-                    header("Location: ../../pages/lista_chamados.php");
-                    exit;
-        header("Location: ../../pages/lista_chamados.php");
-    } 
-} 
-
-
-
-
 ?>
