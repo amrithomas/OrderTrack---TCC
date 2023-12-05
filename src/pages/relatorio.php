b2894f8368ded6ded9e71f26048e21d08f0ae94f
jhordydc <114250931+elderlima7@users.noreply.github.com>
2023-12-05 14:02:05 -0300

    style alert relatorios

--- src/pages/relatorio.php
+++ src/pages/relatorio.php
@@ -32,7 +32,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
     
-    <link rel="stylesheet" href="../../src/styles/relatorios/stylee.css">
+    <link rel="stylesheet" href="../../src/styles/relatorios/styles.css">
 
     <title>Relatórios</title>
 
